d0a2488c1a1f3061b10b1a973e6eb3e13b25f32e
Jason Hanna <jason.hanna01@gmail.com>
2017-10-29 21:00:57 -0500

    add working route method for reading about.html file

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -51,7 +51,20 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about"} = conv) do
-    %{ conv | status: 200, resp_body: "about page content" }
+    file =
+      Path.expand("../../pages", __DIR__)
+      |> Path.join("about.html")
+
+    case File.read(file) do
+      {:ok, content} ->
+        %{ conv | status: 200, resp_body: content}
+
+      {:error, :enoent} ->
+        %{conv | status: 404, resp_body: "File not found"}
+
+      {:error, reason} ->
+        %{ conv| status: 500, resp_body: reason}
+    end
   end
 
   def route(%{ path: path } = conv) do
