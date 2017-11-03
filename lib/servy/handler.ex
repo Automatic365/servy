3c3439c0b2208e43953be23d594731dfccd861f6
Jason Hanna <jason.hanna01@gmail.com>
2017-11-03 12:32:31 -0500

    finshes breaking out modules

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,5 +1,8 @@
 defmodule Servy.Handler do
 
+  import Servy.Plugins, only: [track: 1, rewrite_path: 1, log: 1]
+  import Servy.Parser, only: [parse: 1]
+
 @pages_path Path.expand("../../pages", __DIR__)
 
   def handle(request) do
@@ -10,35 +13,6 @@ defmodule Servy.Handler do
     |> route
     |> track
     |> format_response
-  end
-
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-
-    %{ method: method,
-       path: path,
-       resp_body: "",
-       status: nil
-     }
   end
 
   def route(%{ method: "GET", path: "/wildthings" } = conv) do
