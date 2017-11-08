f72075f92a6f9e43d277f7dc11314d0f6a062902
Jason Hanna <jason.hanna01@gmail.com>
2017-11-08 13:09:25 -0600

    finishes recursion

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,24 +1,40 @@
 defmodule Servy.Parser do
 
-alias Servy.Conv
+  alias Servy.Conv
 
   def parse(request) do
-    [headers, params] = String.split(request, "\n\n")
+    [top, params_string] = String.split(request, "\n\n")
 
-    params = parse_params(params)
+    [request_line | header_lines] = String.split(top, "\n")
 
-    [method, path | request_lines] = String.split( headers, " ")
+    [method, path, _] = String.split(request_line, " ")
+
+    headers = parse_headers(header_lines, %{})
+
+    params = parse_params(headers["Content-Type"], params_string)
+
+    IO.inspect header_lines
 
     %Conv{
-      method: method,
-      params: params,
-      path: path
+       method: method,
+       path: path,
+       params: params,
+       headers: headers
      }
   end
 
-  def parse_params(params) do
-      String.trim(params)
-      |> URI.decode_query
+  def parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
   end
+
+  def parse_headers([], headers), do: headers
+
+  def parse_params("application/x-www-form-urlencoded", params_string) do
+    params_string |> String.trim |> URI.decode_query
+  end
+
+  def parse_params(_, _), do: %{}
 
 end
